58e85733f2669a71ab24413d1fda9e9733cfbffd
Júlio Beledelli <julio_beledelli@hotmail.com>
2022-04-18 20:29:31 -0300

    new model

---
+++ classes/serieRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Series.Interfaces;
+using Series.Collections.Generic;
+
+namespace Serie
+{
+    public class SerieRepositorio : irepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ interfaces/irepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Series.Interfaces
+{
+    public interface irepositorio<T>
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T entedidade);
+        void Exlui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
